147d7bc73c79393db905a1032b57c0fcb64b921f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-02 17:30:46 +0100

    Ch4

--- 4/4-1/Program.cs
+++ 4/4-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace _4_1
 {
@@ -6,7 +7,7 @@ namespace _4_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var doc = new XDocument ();
         }
     }
 }
---
+++ 4/4-2b/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions; 
+using static System.Console; 
+
+namespace _4_2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter your age: "); 
+            string input = ReadLine(); 
+            Regex ageChecker = new Regex(@"^\d+$"); 
+            
+            if(ageChecker.IsMatch(input)) 
+            { 
+                WriteLine("Thank you!"); 
+            } 
+            else 
+            { 
+                WriteLine($"This is not a valid age: {input}"); 
+            }
+        }
+    }
+}
