3786ab1fb78a9e5802274cfbdb61c6120d24dafb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-10 13:03:04 +0100

    Ch9

---
+++ 9/9-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace _9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] {
+                "Michael",
+                "Pam",
+                "Jim",
+                "Dwight",
+                "Angela",
+                "Kevin",
+                "Toby",
+                "Creed"
+            };
+
+            var query = names
+                .Where (name => name.Length > 4)
+                .OrderBy (name => name.Length)
+                .ThenBy (name => name);
+
+            foreach (var item in query)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
---
+++ 9/9-6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace _9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] {
+                "Michael",
+                "Pam",
+                "Jim",
+                "Dwight",
+                "Angela",
+                "Kevin",
+                "Toby",
+                "Creed"
+            };
+
+            var query = names
+                .ProcessSequence()
+                .Where (name => name.Length > 4)
+                .OrderBy (name => name.Length)
+                .ThenBy (name => name);
+
+            foreach (var item in query)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
