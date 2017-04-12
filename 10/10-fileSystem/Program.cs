a425cdc48a47608141d60e6a1529b073a7487f0a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-04-12 06:54:31 +0100

    Ch10-fileSystems

---
+++ 10/10-fileSystem/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using static System.IO.Directory;
+
+namespace _10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define a directory path
+            // string dir = @"C:\Code\Ch10_Example"; // Windows
+            string dir = @"/Users/mick/Documents/dotnet/dotnetTut/10/10-1/Ch10_Example/"; // macOS
+            // check if it exists
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+            // create a directory
+            CreateDirectory(dir);
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+            // delete a directory
+            Delete(dir);
+            WriteLine($"Does {dir} exist? {Exists(dir)}");
+
+
+            // string textFile = @"C:\Code\Ch10.txt"; // Windows
+            // string backupFile = @"C:\Code\Ch10.bak"; // Windows
+            string textFile = @"/Users/mick/Documents/dotnet/dotnetTut/10/10-1/Ch10.txt"; // macOS
+            string backupFile = @"/Users/mick/Documents/dotnet/dotnetTut/10/10-1/Ch10.bak"; // macOS
+            // check if a file exists
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+            // create a new text file and write a line to it
+            StreamWriter textWriter = File.CreateText(textFile);
+            textWriter.WriteLine("Hello, C#!");
+            textWriter.Dispose();
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+            // copy a file and overwrite if it already exists
+            File.Copy(textFile, backupFile, true);
+            WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+            // delete a file
+            File.Delete(textFile);
+            WriteLine($"Does {textFile} exist? {File.Exists(textFile)}");
+            // read from a text file
+            StreamReader textReader = File.OpenText(backupFile);
+            WriteLine(textReader.ReadToEnd());
+            textReader.Dispose();
+
+            WriteLine($"File Name: {Path.GetFileName(textFile)}");
+            WriteLine($"File Name without Extension: {Path.GetFileNameWithoutExtension(textFile)}");
+            WriteLine($"File Extension: {Path.GetExtension(textFile)}");
+            WriteLine($"Random File Name: {Path.GetRandomFileName()}");
+            WriteLine($"Temporary File Name: {Path.GetTempFileName()}");
+
+            var info = new FileInfo(backupFile);
+            WriteLine($"{backupFile} contains {info.Length} bytes.");
+            WriteLine($"{backupFile} was last accessed {info.LastAccessTime}.");
+            WriteLine($"{backupFile} has readonly set to {info.IsReadOnly}.");
+    
+        }
+    }
+}
---
+++ 10/10-streams/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using static System.Console;
+
+namespace _10_streams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define an array of strings
+            string[] callsigns = new string[] { "Husker", "Starbuck",
+            "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack" };
+            // define a file to write to using a text writer helper
+            string textFile = @"/Users/mick/Documents/dotnet/dotnetTut/10/10-streams/Ch10_Streams.txt";
+            // string textFile = @"C:\Code\Ch10_Streams.txt"; // Windows
+            StreamWriter text = File.CreateText(textFile);
+            // enumerate the strings writing each one to the stream
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+            text.Dispose(); // close the stream
+            // output all the contents of the file to the Console
+            WriteLine($"{textFile} contains {new FileInfo(textFile).Length} bytes.");
+            WriteLine(File.ReadAllText(textFile));
+            // define a file to write to using the XML writer helper
+            string xmlFile = @"/Users/mick/Documents/dotnet/dotnetTut/10/10-streams/Ch10_Streams.xml";
+            // string xmlFile = @"C:\Code\Ch10_Streams.xml";
+            FileStream xmlFileStream = File.Create(xmlFile);
+            XmlWriter xml = XmlWriter.Create(xmlFileStream,
+            new XmlWriterSettings { Indent = true });
+            // write the XML declaration
+            xml.WriteStartDocument();
+            // write a root element
+            xml.WriteStartElement("callsigns");
+            // enumerate the strings writing each one to the stream
+            foreach (string item in callsigns)
+            {
+                xml.WriteElementString("callsign", item);
+            }
+            // write the close root element
+            xml.WriteEndElement();
+            xml.Dispose();
+            xmlFileStream.Dispose();
+            // output all the contents of the file to the Console
+            WriteLine($"{xmlFile} contains {new FileInfo(xmlFile).Length} bytes.");
+            WriteLine(File.ReadAllText(xmlFile));
+        }
+    }
+}
