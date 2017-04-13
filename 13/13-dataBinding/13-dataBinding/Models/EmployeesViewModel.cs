02fad0212a37fb95620528973d2af13ff1b4637d
mc-carthy <mccarthy.mick0012@gmail.com>
2017-04-13 15:47:14 +0100

    13-dataBinding

---
+++ 13/13-dataBinding/13-dataBinding/Models/EmployeesViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_dataBinding.Models
+{
+    public class EmployeesViewModel
+    {
+        public HashSet<Employee> Employees { get; set; }
+
+        public EmployeesViewModel()
+        {
+            Employees = new HashSet<Employee>();
+
+            Employees.Add(new Employee
+            {
+                EmployeeID = 1,
+                FirstName = "Alice",
+                LastName = "Smith",
+                DOB = new DateTime(1972, 1, 27),
+                Salary = 34000M
+            });
+
+            Employees.Add(new Employee
+            {
+                EmployeeID = 2,
+                FirstName = "Bob",
+                LastName = "Jones",
+                DOB = new DateTime(1965, 4, 13),
+                Salary = 64000M
+            });
+        }
+    }
+}
